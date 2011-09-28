3fe25e13e35c388c51ef58881b97c8754b710415
Martin Bukatovic <martin.bukatovic@gmail.com>
2011-09-28 11:19:03 +0200

    add init command to text-sync

--- text-sync.py
+++ text-sync.py
@@ -1,8 +1,26 @@
 #!/usr/bin/env python2.7
 # -*- coding: utf8 -*-
 
+"""
+Simple shell wrapper syncing 'text directories' with rsync.
+Where 'text directory' contains only big, binary and hardly
+changed data (images, pdf files, ...).
+"""
+
+CONF_TEMPLATE = """[dirs]
+local = %(local)s/
+remote = %(remote)s
+"""
+
+# TODO:
+# add cmd abritriary arg. handling for rsync
+# add man config spec
+# add logging
+# add support for symlinked dirs
+
 
 import sys
+import os
 from argparse import ArgumentParser
 import ConfigParser
 import shlex
@@ -32,6 +50,20 @@ def info(conf, args):
     print "local  dir: %s" % conf.local
     print "remote dir: %s" % conf.remote
 
+def init(args):
+    """create configuration"""
+    content = CONF_TEMPLATE % {"local": args.text_dir, "remote":""}
+    if args.debug:
+        print content
+        return
+    try:
+        fd = os.open(".text-sync", os.O_WRONLY|os.O_CREAT|os.O_EXCL)
+    except OSError:
+        sys.stderr.write("config file '.text-sync' already exists\n")
+        return 1
+    os.write(fd, content)
+    os.close(fd)
+
 def main():
     parser = ArgumentParser(description='Simple text sync tool using rsync.')
     parser.add_argument('-d',
@@ -42,18 +74,23 @@ def main():
 
     # define subcommands
     commands = {}
-    for cmd in ('info', 'push', 'pull'):
+    for cmd in ('init', 'info', 'push', 'pull'):
         cmd_parser = subparsers.add_parser(cmd, help=globals().get(cmd).__doc__)
         cmd_parser.set_defaults(func=globals().get(cmd))
         commands[cmd] = cmd_parser
+    commands['init'].add_argument('text_dir', help='directory with text repo')
 
-    # parse args. and conf. file
     args = parser.parse_args()
+
+    # init hack
+    if args.func.__name__ == 'init':
+        return args.func(args)
+
+    # parse conf. file
     conf = ConfigParser.RawConfigParser()
     if not conf.read('.text-sync'):
         sys.stderr.write("no text-sync directory\n")
         return 2
-
     # read mandatory config options
     try:
         conf.local = conf.get("dirs", "local")
