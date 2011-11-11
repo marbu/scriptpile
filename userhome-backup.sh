515fd2eed73e136655a6a65dbb7027a18a0f3d2a
Martin Bukatovic <martin.bukatovic@gmail.com>
2011-11-11 16:12:05 +0100

    polish run_backup, add disk usage stats

--- userhome-backup.sh
+++ userhome-backup.sh
@@ -3,46 +3,103 @@
 # by marbu, 2011
 
 # configuration (TODO: move outside)
-HOME_TO_BACKUP=/home/martin/tmp
-REMOTE=scp://skoll/home/backup/dione/martin/
-ENC_KEY=FB0CCD8C
-SGN_KEY=16A500AE
+HOME_TO_BACKUP=/home/martin/
+REMOTE=scp://skoll/dione/martin/
+ENC_KEY=458EB91A
+SGN_KEY=4A354563
+
+help() {
+  echo -e "duplicity home backup script\n"
+  echo -e "Usage: $(basename $0) <command>\n"
+  echo """Commands:
+  show          show configuration
+  du            show disk usage stats
+  exlist        show list of excluded files
+  run           run backup
+  help          this text
+"""
+}
 
 # "do not backup" file indicates:
-#  * this directory should be excluded from backup 
+#  * either entire directory should be excluded from backup 
 #  * or contains filenames of files excluded from backup
 DNB_FN=".do_not_backup"
 
-# create new exclude list based
-# on DNB files
+# create new exclude list based on DNB files
+# arg: dir to search in
 make_exclude_list() {
-  LIST=$(find $1 -name $DNB_FN)
+  SOURCE_D=$1
+  LIST=$(find $SOURCE_D -name $DNB_FN)
   for i in $LIST; do
     DIRNAME=${i%$DNB_FN}
     if [[ -s $i ]]; then
       while read LINE; do
-        echo "- $DIRNAME$LINE"
+        echo "$DIRNAME$LINE"
       done < $i
     else
-      echo "- ${DIRNAME}"
+      echo "${DIRNAME}"
     fi
   done
 }
 
+run_backup() {
+  SOURCE_D=$1
+  TARGET_D=$2
+
+  # create list of excluded filenames
+  # supposing /tmp is on encrypted volume
+  EXCL_FLIST=$(mktemp)
+  if [[ $DEBUG ]]; then
+    make_exclude_list $SOURCE_D >&2
+  else
+    make_exclude_list $SOURCE_D | sed 's/^/- /' > $EXCL_FLIST
+  fi
+
+  $DEBUG /usr/bin/duplicity \
+    --encrypt-key $ENC_KEY \
+    --sign-key $SGN_KEY \
+    --exclude-filelist $EXCL_FLIST \
+    $SOURCE_D $TARGET_D
+
+  rm $EXCL_FLIST
+}
+
+show_conf() {
+  echo "local  dir: $HOME_TO_BACKUP"
+  echo "remote dir: $REMOTE"
+}
+
+show_du() {
+  SOURCE_D=$1
+  # show how much space is being backed up
+  EXCL_FLIST=$(mktemp)
+  make_exclude_list $SOURCE_D > $EXCL_FLIST
+  du -shc $(cat $EXCL_FLIST)
+  # TODO: doesn't work
+  # du -s --exclude-from=$EXCL_FLIST $SOURCE_D
+  rm $EXCL_FLIST
+}
 
 #
 # main
 #
 
-# create exclude filelist
-# supposing /tmp is on encrypted volume
-EXCLUDE_LIST_FILE=$(mktemp)
-make_exclude_list $HOME_TO_BACKUP > $EXCLUDE_LIST_FILE
+if [[ $# -eq 0 ]]; then
+  help
+  exit
+fi
 
-# run backup
-duplicity \
-  --encrypt-key $ENC_KEY --sign-key $SGN_KEY \
-  --exclude-filelist $EXCLUDE_LIST_FILE \
-  $HOME_TO_BACKUP $REMOTE
+# debug mode
+if [[ $1 = "-d" ]]; then
+  DEBUG=echo
+  shift
+fi
 
-rm $EXCLUDE_LIST_FILE
+case $1 in
+  run)    run_backup $HOME_TO_BACKUP $REMOTE;;
+  show)   show_conf;;
+  du)     show_du $HOME_TO_BACKUP;;
+  exlist) make_exclude_list $HOME_TO_BACKUP;;
+  help)   help;;
+  *)      help; exit 1;;
+esac
