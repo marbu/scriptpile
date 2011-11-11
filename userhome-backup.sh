096e45e199fc7679e07508577844ea3bbde80e21
Martin Bukatovic <martin.bukatovic@gmail.com>
2011-11-11 01:54:56 +0100

    initial version of userhome backup script

--- userhome-backup.sh
+++ userhome-backup.sh
@@ -2,6 +2,12 @@
 # simple script to backup home directory of given user
 # by marbu, 2011
 
+# configuration (TODO: move outside)
+HOME_TO_BACKUP=/home/martin/tmp
+REMOTE=scp://skoll/home/backup/dione/martin/
+ENC_KEY=FB0CCD8C
+SGN_KEY=16A500AE
+
 # "do not backup" file indicates:
 #  * this directory should be excluded from backup 
 #  * or contains filenames of files excluded from backup
@@ -15,12 +21,28 @@ make_exclude_list() {
     DIRNAME=${i%$DNB_FN}
     if [[ -s $i ]]; then
       while read LINE; do
-        echo $DIRNAME$LINE
+        echo "- $DIRNAME$LINE"
       done < $i
     else
-      echo ${DIRNAME}
+      echo "- ${DIRNAME}"
     fi
   done
 }
 
-make_exclude_list $1
+
+#
+# main
+#
+
+# create exclude filelist
+# supposing /tmp is on encrypted volume
+EXCLUDE_LIST_FILE=$(mktemp)
+make_exclude_list $HOME_TO_BACKUP > $EXCLUDE_LIST_FILE
+
+# run backup
+duplicity \
+  --encrypt-key $ENC_KEY --sign-key $SGN_KEY \
+  --exclude-filelist $EXCLUDE_LIST_FILE \
+  $HOME_TO_BACKUP $REMOTE
+
+rm $EXCLUDE_LIST_FILE
