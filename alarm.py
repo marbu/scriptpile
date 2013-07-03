2bde4b349e1c347f2bbe178293f90197a4681b68
Martin Bukatovic <martin.bukatovic@gmail.com>
2013-07-03 07:45:32 +0200

    refactor alarm.py (for extending timespec later)

--- alarm.py
+++ alarm.py
@@ -6,7 +6,7 @@ Simple alarm sched script for music player daemon (MPD).
 
 Following example:
 
-    $ alarm.py -d --time 20:05 --period 20 --volume 10,50,70
+    $ alarm.py 20:05 -d --period 20 --volume 10,50,70
 
 will do roughly this:
 
@@ -43,14 +43,12 @@ def run_at(time_str, sched_cmd, debug=False):
         output = check_output(cmd, shell=True)
     return output
 
-def sched_alarm(when_str, volume_list=None, period=1, debug=False):
+def sched_alarm(time_obj, volume_list=None, period=1, debug=False):
     """
     Schedule alarm at time given by when_str.
     """
     volume_list = volume_list or []
     # schedule alarm
-    time_st  = time.strptime(when_str, "%H:%M")
-    time_obj = datetime.datetime(2012, 1, 1, time_st.tm_hour, time_st.tm_min, 0)
     run_at(time_obj.strftime("%H:%M"), CMD_ALARM, debug)
     # gradually increase volume if required
     time_delta = datetime.timedelta(seconds=period*60)
@@ -58,11 +56,21 @@ def sched_alarm(when_str, volume_list=None, period=1, debug=False):
         run_at(time_obj.strftime("%H:%M"), CMD_VOLUME_UP % volume, debug)
         time_obj = time_obj + time_delta
 
+def parse_time_spec(args):
+    """
+    Process args and return time object when to run the alarm.
+
+    @param args: arg list from main
+    @return    : time object (when to run the alarm)
+    """
+    # TODO: support multiple timeformats
+    time_spec = args[0]
+    time_st  = time.strptime(time_spec, "%H:%M")
+    time_obj = datetime.datetime(2012, 1, 1, time_st.tm_hour, time_st.tm_min, 0)
+    return time_obj
+
 def main():
-    op = OptionParser(usage="usage: %prog [options]")
-    op.add_option("-t", "--time",
-        action="store",
-        help="when to run alarm (HH:MM)")
+    op = OptionParser(usage="usage: %prog time [options]")
     op.add_option("-d", "--debug",
         action="store_true",
         help="debug mode")
@@ -74,16 +82,19 @@ def main():
         action="store",
         type="int",
         help="period (min) of volume ups")
+
     op.set_defaults(period=1)
     op.set_defaults(volume="")
     opts, args = op.parse_args()
 
-    if opts.time is not None:
-        volume_list = [int(vol) for vol in opts.volume.split(",")]
-        sched_alarm(opts.time, volume_list, opts.period, opts.debug)
+    if len(args) > 0:
+        time_obj = parse_time_spec(args)
     else:
-        sys.stderr.write("Error: time not specified, use --time to set it.\n")
+        sys.stderr.write("Error: time not specified.\n")
         return 1
+
+    volume_list = [int(vol) for vol in opts.volume.split(",")]
+    sched_alarm(time_obj, volume_list, opts.period, opts.debug)
 
 if __name__ == '__main__':
     sys.exit(main())
