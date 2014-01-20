4998c95b9ec07d11dece9f3dee16ef19cac72b02
Martin Bukatovic <martin.bukatovic@gmail.com>
2014-01-21 00:06:10 +0100

    speed-up java lsof wrapper

--- jlsof.sh
+++ jlsof.sh
@@ -7,8 +7,18 @@ if [[ "$1" =~ ^--?h(elp)?$ ]]; then
   exit
 fi
 
+# at first load all java PIDs into asociative array to make this faster
+declare -A JAVA_PIDS
+while read PID CLASS; do
+  JAVA_PIDS[$PID]=$CLASS
+done < <(jps)
+
 lsof $@ | grep ^java | {
 while read IGNORE PID REST; do
-  CLASS=$(jps | grep "^${PID}\ " | cut -d' ' -f2)
+  CLASS=${JAVA_PIDS[$PID]}
+  # minor case: handle new process (created after init of JAVA_PIDS array)
+  if [[ ! $CLASS ]]; then
+    CLASS=$(jps | grep "^${PID}\ " | cut -d' ' -f2)
+  fi
   echo $CLASS $PID $REST
 done; }
