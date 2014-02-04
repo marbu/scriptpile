98e224d94d24f5f1b57a5c397b8b26dc954081c9
Martin Bukatovic <martin.bukatovic@gmail.com>
2014-02-05 00:26:59 +0100

    jps.sh: don't show entry for jps process

--- jps.sh
+++ jps.sh
@@ -6,11 +6,20 @@ if [[ "$1" =~ ^--?h(elp)?$ ]]; then
   exit
 fi
 
-jps $@ | {
+PID_FILE=$(mktemp /tmp/jps.XXXXXXXX.pid)
+
+{ jps $@ & echo $! > $PID_FILE; } | {
+JPS_PID=$(cat $PID_FILE);
 while read PID CLASS; do
+  # don't show jps process itself
+  if [[ $JPS_PID = $PID ]]; then
+    continue
+  fi
   USER=$(ps -p $PID -o ruser | tail -1)
   # when the process already doesn't exists, USER contains 'RUSER' string
   if [[ $USER != "RUSER" ]]; then
     echo -e "$PID\t$USER\t$CLASS"
   fi
 done; } | column -t
+
+rm $PID_FILE
