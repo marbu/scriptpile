e611bcc3624223c252e0cfec1a3d0837922303f0
Martin Bukatovic <martin.bukatovic@gmail.com>
2014-06-20 01:34:24 +0200

    git-pwd: pwd wrt to working tree

---
+++ git-pwd.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# git-pwd: print working directory wrt to the location of git working tree
+
+# try to get root directory of git repo, exit if no repo found                
+if ! GIT_ROOT=$(git rev-parse --show-toplevel); then                          
+  exit 1                                                                    
+fi 
+
+echo ${PWD#"${GIT_ROOT}/"}
