b0f3db5dbffe28149b39d509bee0fa45cbbac152
Martin Bukatovic <martin.bukatovic@gmail.com>
2014-06-20 22:27:45 +0200

    git-pwd.sh whitespace fix

--- git-pwd.sh
+++ git-pwd.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # git-pwd: print working directory wrt to the location of git working tree
 
-# try to get root directory of git repo, exit if no repo found                
-if ! GIT_ROOT=$(git rev-parse --show-toplevel); then                          
-  exit 1                                                                    
-fi 
+# try to get root directory of git repo, exit if no repo found
+if ! GIT_ROOT=$(git rev-parse --show-toplevel); then
+  exit 1
+fi
 
 if [[ "$PWD" = "$GIT_ROOT" ]]; then
   echo "/"
