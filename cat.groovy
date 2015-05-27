4899b5e5e6db61bbc5c3f33a8bb2bccf8e0cc55f
Martin Bukatovic <martin.bukatovic@gmail.com>
2015-05-27 23:20:14 +0200

    update groovy cat: class is not needed

--- cat.groovy
+++ cat.groovy
@@ -1,35 +1,29 @@
 #!/usr/bin/groovy
 
 /*
- * Simple cat implementation in Groovy.
+ * Read and print content of a file object.
  */
-class Cat {
+static void cat(file) {
+    file.eachLine { line -> println(line) }
+}
 
-    /*
-     * Read and print content of a file object.
-     */
-    static void cat(file) {
-        file.eachLine { line -> println(line) }
-    }
-
-    /*
-     * Method of main function.
-     */
-    static void main(String[] args) {
-        int retcode = 0
-        if (args.size() == 0) {
-            // this will catch keyborad exception and return 130 by default
-            cat(new InputStreamReader(System.in))
-            System.exit(retcode)
-        }
-        args.each { filename ->
-            try {
-                cat(new File(filename))
-            } catch (java.io.IOException ex) {
-                System.err.println(ex)
-                retcode = 1
-            }
-        }
+/*
+ * Method of main function.
+ */
+static void main(String[] args) {
+    int retcode = 0
+    if (args.size() == 0) {
+        // this will catch keyborad exception and return 130 by default
+        cat(new InputStreamReader(System.in))
         System.exit(retcode)
     }
+    args.each { filename ->
+        try {
+            cat(new File(filename))
+        } catch (java.io.IOException ex) {
+            System.err.println(ex)
+            retcode = 1
+        }
+    }
+    System.exit(retcode)
 }
