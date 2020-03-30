bd4cca2c71df04f5e05972ead531bd66219efab3
Martin Bukatovic <martin.bukatovic@gmail.com>
2020-03-30 21:27:05 +0200

    fix whitespace typos

--- libvirt-btrfs-disable-cow.sh
+++ libvirt-btrfs-disable-cow.sh
@@ -30,11 +30,11 @@ $DEBUG chattr +C "${IMAGES_DIR}"
 for IMG in "${IMAGES_DIR}"/*.qcow*; do
   if lsof "${IMG}" > /dev/null; then
     echo "image $IMG is being used, skipping"
-	continue
+    continue
   fi
   if lsattr "${IMG}" | cut -d' ' -f1 | grep C > /dev/null; then
     echo "image $IMG already has C attribute, skipping"
-	continue
+    continue
   fi
   TMP_FILE="${TMP_DIR}"/$(basename "$IMG")
   $DEBUG mv "${IMG}" "${TMP_FILE}"
