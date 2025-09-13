9f233d09bd63b9daaf2782efa6c0e746caa9bcfb
Martin Bukatovic <martin.bukatovic@gmail.com>
2025-09-13 15:20:15 +0200

    add git-ammend-date-now

--- git-ammend-date-now.sh
+++ git-ammend-date-now.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
-export GIT_COMMITTER_DATE=$(date +'%b %e %T %Y %z') 
+
+show_help()
+{
+  echo "Usage: $(basename "$0") [TIMESTAMP]"
+  echo "Where TIMESTAMP is something like 'Sep 13 13:13:33 2025 +0200'"
+}
+
+if [[ $1 = "-h" ]]; then
+  show_help
+  exit
+fi
+
+if [[ $# = 0 ]]; then
+  export GIT_COMMITTER_DATE=$(date +'%b %e %T %Y %z')
+else
+  export GIT_COMMITTER_DATE=$1
+fi
+
 git commit --amend --no-edit --date "${GIT_COMMITTER_DATE}"
